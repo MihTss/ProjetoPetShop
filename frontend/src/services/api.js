70bea07826588b3221c2bd64cdda7dada29efc93
MihTss <102559859+MihTss@users.noreply.github.com>
2024-06-13 17:21:43 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const urlBack = "http://localhost:3000"
+const urlBack = "3.14.142.239:3000"
 
 //Configuração do axios para o url correto, bem como adição do header que define o tipo de dado que será enviado nas requisições.
 const api = axios.create({
